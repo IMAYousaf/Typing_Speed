b629120ad639d38c9a644e32e9cfc3458d66bf02
IMAYousaf <IMAYousaf@gmail.com>
2016-12-24 21:03:17 -0800

    Trying WPM

--- main.py
+++ main.py
@@ -6,13 +6,15 @@ def main():
     text = open("The_Arabian_Nights.txt").readlines()
     speed_test_selection = random.choice(text)
 
+    correct = "\"" + speed_test_selection + "\""
+
     print("You will be tested based upon how fast you can type a randomly selected line from \"The Arabian Nights\"")
     continuation = input("Push ENTER to continue.")
     print("You will have a 15 second delay to read the passage, and to get ready to type.")
     print("Afterwards, a timer will count down from 3 and prompt you to begin.")
     continuation = input("Push ENTER to continue.")
     print("This is going to be your tested passage:")
-    print(speed_test_selection)
+    print("\"" + speed_test_selection + "\"")
     time.sleep(15)
     print("15 Seconds have Elapsed.")
     print("3")
@@ -27,12 +29,14 @@ def main():
 
     end = time.time()
 
-    if assessment == speed_test_selection:
+    if assessment == correct:
         print("Congratulations!")
         print(end-begin + "seconds have elapsed. You are so slow!")
+        #print("You type as a speed of " + str((WORDS/((end-begin)/60))) + " words per minute."
     else:
-        print("Learn to type!")
+        print("Incorrect! Learn to type!")
         print(str(end-begin) + " seconds have elapsed. You are so slow!")
+        #print("You type as a speed of " + str((WORDS/((end-begin)/60))) + " words per minute."
 
 
 main()
