dcd42685ede64e63a3f6d4639e3e46de4c497a10
IMAYousaf <IMAYousaf@gmail.com>
2016-12-28 22:29:11 -0800

    Shortened main.py

--- main.py
+++ main.py
@@ -1,49 +1,39 @@
 import random
 import time
-
+import difflib
 
 def main():
-    text = open("The_Arabian_Nights.txt").readlines()
-    speed_test_selection = random.choice(text).strip()
-    word_count = len(speed_test_selection.split())
-
-    print("You will be tested based upon how fast you can type a randomly selected line from \"The Arabian Nights\"")
-    continuation = input("Push ENTER to continue.")
-    print("You will have a 15 second delay to read the passage, and to get ready to type.")
-    print("Afterwards, a timer will count down from 3 and prompt you to begin.")
-    print("Your passage will be nestled between two sets of parentheses. Punctuation counts, but don't type those out!")
-    continuation = input("Push ENTER to continue.")
-    print("This is going to be your tested passage:")
-    print("\"" + speed_test_selection + "\"")
-    time.sleep(15)
-    print("15 Seconds have Elapsed.")
-    print("3")
-    time.sleep(1)
-    print("2")
-    time.sleep(1)
-    print("1")
-    print("Begin!")
+    choice = input("Type in \"EASY\" or \"HARD\" based upon your desired difficulty level:")
+    if choice in {"EASY", "easy"}:
+        text = open("The_Arabian_Nights.txt").readlines()
+        selection = random.choice(text).strip()
+        word_count = len(selection.split())
+    elif choice in {"HARD", "hard"}:
+        with open("The_Arabian_Nights.txt") as paragraphs:
+            text = paragraphs.read()
+            selection = text.split('\n\n')
+            selection = random.choice(selection)
+            word_count = len(selection.split())
+    print(selection)
     begin = time.time()
     assessment = input()
     end = time.time()
-    elapsed = end-begin
-    if assessment == speed_test_selection:
-        print("Congratulations!")
+    if assessment == selection:
+        print("Correct")
     else:
-        print("Incorrect!")
-        print("Learn to type!")
-    print("Here are your performance stats:")
-    print(elapsed, " seconds have elapsed. You are so slow!")
-    print("You type at a speed of ", 60 * word_count / elapsed, " words per minute.")
+        print("Incorrect")
+    elapsed = end - begin
+    print(elapsed, "seconds have elapsed.")
+    print ("You type at a speed of", 60 * word_count / elapsed, "words per minute.")
+    file = open("test.txt", "w")
+    file.write(selection)
+    file.write("\n\n")
+    file.write(assessment)
+    d = difflib.Differ()
+    difference = d.compare(selection.split(), assessment.split())
+    file.write("\n")
+    file.write(str(''.join(difference)))
+    s = difflib.SequenceMatcher(None, selection, assessment)
+    print("You were", s.ratio() * 100, "% correct")
 
 main()
-
-#setting up accuracy test
-'''
-    for char in assessment:
-        if assessment[1] != speed_test_selection[1]:
-            mistake += 1
-            return
-
-    print(str(mistake))
-'''
---
+++ test.py
@@ -0,0 +1,52 @@
+'''
+import random
+import time
+
+
+def main():
+    text = open("The_Arabian_Nights.txt").readlines()
+    speed_test_selection = random.choice(text).strip()
+    word_count = len(speed_test_selection.split())
+
+    print("You will be tested based upon how fast you can type a randomly selected line from \"The Arabian Nights\"")
+    continuation = input("Push ENTER to continue.")
+    print("You will have a 15 second delay to read the passage, and to get ready to type.")
+    print("Afterwards, a timer will count down from 3 and prompt you to begin.")
+    print("Your passage will be nestled between two sets of parentheses. Punctuation counts, but don't type those out!")
+    continuation = input("Push ENTER to continue.")
+    print("This is going to be your tested passage:")
+    print("\"" + speed_test_selection + "\"")
+    time.sleep(15)
+    print("15 Seconds have Elapsed.")
+    print("3")
+    time.sleep(1)
+    print("2")
+    time.sleep(1)
+    print("1")
+    print("Begin!")
+    begin = time.time()
+    assessment = input()
+    end = time.time()
+    elapsed = end-begin
+    if assessment == speed_test_selection:
+        print("Congratulations!")
+    else:
+        print("Incorrect!")
+        print("Learn to type!")
+    print("Here are your performance stats:")
+    print(elapsed, " seconds have elapsed. You are so slow!")
+    print("You type at a speed of ", 60 * word_count / elapsed, " words per minute.")
+
+main()
+
+'''
+
+#setting up accuracy test
+'''
+    for char in assessment:
+        if assessment[1] != speed_test_selection[1]:
+            mistake += 1
+            return
+
+    print(str(mistake))
+'''
